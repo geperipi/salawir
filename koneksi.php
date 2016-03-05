e60677e3117347b8e1bfbb6427962018e2d2508f
geperipi <gpowa@yahoo.com>
2016-03-05 22:30:19 +0700

    sd

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,25 @@
 <?php
-$host="localhost";
-$user="root";
-$pass="";
-$db="torangbatamang";
+$host=getenv('OPENSHIFT_MYSQL_DB_HOST');
+$user=getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$password=getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$database=('OPENSHIFT_GEAR_NAME');
 
-$konek= mysql_connect($host,$user,$pass) or die ('Koneksi gagal');
-mysql_select_db($db);
+if ($user==''){
+	$user="adminSeC3tcL";
+}
+if ($password==''){
+	$password="FqrJv7RQFSXM";
+}
+if($database==''){
+	$database="myphpapp";
+}
+$koneksi= mysql_connect($host,$user,$password);
+mysql_select_db($database,$koneksi);
+//cek koneksi
+if ($koneksi){
+	echo "koneksi berhasil";
+}
+else{
+	echo "koneksi gagal";
+}
 ?>
